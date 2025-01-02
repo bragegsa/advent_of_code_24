9cf8380aaf8fae5b23e5b66dbb62b021406503e1
bragega <brage.samsonsen@gmail.com>
2025-01-02 11:57:31 +0100

    Added some cleanup and the possibility to run all tasks in one script

---
+++ all_days.py
@@ -0,0 +1,5 @@
+from day1 import run_day1
+
+if __name__ == "__main__":
+
+    run_day1()
--- day1.py
+++ day1.py
@@ -1,10 +1,16 @@
 import pandas as pd
 from modules import get_list, get_distance, import_data, similarity_score
 
-data = import_data()
+def run_day1():
 
-list1 = get_list(data, 'List 1')
-list2 = get_list(data, 'List 2')
+    data = import_data()
 
-print(f'Distance is: {get_distance(list1, list2)}')
-print(f'Similarity score is: {similarity_score(list1, list2)}')
+    list1 = get_list(data, 'List 1')
+    list2 = get_list(data, 'List 2')
+
+    print(f'Distance is: {get_distance(list1, list2)}')
+    print(f'Similarity score is: {similarity_score(list1, list2)}')
+
+if __name__ == "__main__":
+    
+    run_day1()
