5689c5ca3fb2d222030673a07dc2bc58f1ea56b4
bragega <brage.samsonsen@gmail.com>
2025-01-02 10:31:56 +0100

    Finished day 1 part 2 and added gitignore

--- day1.py
+++ day1.py
@@ -1,10 +1,10 @@
 import pandas as pd
-from modules import get_list, get_distance, get_distance_list, import_data
+from modules import get_list, get_distance, import_data, similarity_score
 
 data = import_data()
 
 list1 = get_list(data, 'List 1')
 list2 = get_list(data, 'List 2')
 
-distance = get_distance(get_distance_list(list1, list2))
-print(distance)
+print(f'Distance is: {get_distance(list1, list2)}')
+print(f'Similarity score is: {similarity_score(list1, list2)}')
