c49ea1dd09e1fcd9f5aac6c720a60611d19716e0
bragega <brage.samsonsen@gmail.com>
2025-01-03 12:39:29 +0100

    Finished day 3 part 2

--- modules/day3_lib.py
+++ modules/day3_lib.py
@@ -4,6 +4,12 @@ import re
 def import_data_day3(path: str = 'inputs/day3.txt') -> str:
     '''
         Returns content of a txt file as a string
+
+        Args:
+            path: the path to input file
+
+        Returns:
+            data: A string of data from input file
     '''
 
     f = open(path, "r")
@@ -12,14 +18,25 @@ def import_data_day3(path: str = 'inputs/day3.txt') -> str:
 
     return data
 
-def filter_data(data: list) -> list:
+def filter_data(data: list, extra_instructions: bool = False) -> list:
     '''
         Takes a string of data and finds all operations with
         respect to the criteria of the daily task
+
+        Args:
+            data: A string of data
+            extra_instructions: Enabels the use of more than the mul operation
+
+        Returns:
+            list_of_operations: A list of operations
     '''
 
     list_of_operations = []
-    regex = 'mul\([0-9]{1,3}\,[0-9]{1,3}\)'
+
+    if extra_instructions:
+        regex = 'mul\([0-9]{1,3}\,[0-9]{1,3}\)|do\(\)|don\'t\(\)'
+    else:
+        regex = 'mul\([0-9]{1,3}\,[0-9]{1,3}\)'
 
     mul_operations = re.findall(regex, data)
     list_of_operations.extend(mul_operations)
@@ -29,21 +46,38 @@ def filter_data(data: list) -> list:
 def compute_mul(operations: list) -> int:
     '''
         Computes the sum of the multiplication operations
+
+        Args:
+            operations: A list of operations to be performed
+
+        Returns:
+            sum: The sum of all operations
     '''
 
     sum = 0
     regex = '[0-9]{1,3}'
+    skip = False
 
     for operation in operations:
 
         product = 1
-        numbers = re.findall(regex, operation)
+        numbers = re.findall(regex, operation)    
 
-        for number in numbers:
+        if operation == 'do()':
+        
+            skip = False
 
-            product *= int(number)
-         
-        sum += product        
+        elif operation == 'don\'t()':
+
+            skip = True
+
+        elif not skip:
+
+            for number in numbers:
+
+                product *= int(number)
+        
+            sum += product    
 
     return sum
 
@@ -58,6 +92,10 @@ def run_day3():
     operations = filter_data(data)
     sum = compute_mul(operations)
     print(f'The sum of all Mul computations is: {sum}')
+
+    operations_part2 = filter_data(data, extra_instructions=True)
+    sum_part2 = compute_mul(operations_part2)
+    print(f'The sum of all Mul computations is: {sum_part2}')
    
 if __name__ == "__main__":
 
