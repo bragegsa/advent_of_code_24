ec2f119f9118ec6a080908cbf7fc367a7b377f71
bragega <brage.samsonsen@gmail.com>
2025-01-08 14:37:36 +0100

    Finished day 5 part 2

--- modules/day5_lib.py
+++ modules/day5_lib.py
@@ -40,7 +40,7 @@ def check_updates(updates: list, rules: dict) -> list:
 
         Args:
             updates: list of updates
-            rules_dict: dictionary containing rules for each number
+            rules: dictionary containing rules for each number
 
         Returns:
             result_list: list of correct updates
@@ -95,6 +95,80 @@ def result_sum(updates: list, result_list: list) -> int:
 
     return result_sum
 
+def incorrect_updates(updates: list, result_list: list) -> list:
+
+    remaining_updates = []
+
+    for i, result in enumerate(result_list):
+
+        if not result:
+
+            remaining_updates.append(updates[i])
+
+    return remaining_updates
+
+def fix_incorrect_update(update: list, rules: dict) -> list:
+    '''
+        !Recursive function!
+
+        Returns a ordered list based on rules.
+
+        Args:
+            update: list of pages
+            rules: dictionary containing rules for each number
+
+        Returns:
+            new_update: list containing ordered update based on rules
+    '''
+
+    reversed_update = update[::-1]
+    new_update = reversed_update
+
+    for i in range(len(update)):
+
+        for j in range(i+1, len(update)):
+
+            if reversed_update[i] in rules.keys():
+                if reversed_update[j] in rules[reversed_update[i]]:
+
+                    first_priority_page = reversed_update[i]
+                    last_priority_page = reversed_update[j]
+
+                    new_update[j] = first_priority_page
+                    new_update[i] = last_priority_page
+
+                    new_update = new_update[::-1]
+                    new_update = fix_incorrect_update(new_update, rules)
+
+                    return new_update
+            
+
+    return new_update[::-1]
+
+def fix_incorrect_updates(remaining_updates: list, rules:list) -> tuple[list, list]:
+    '''
+        Returns two lists. One is the correctly ordered updates and the other is
+        the list of bools showing correctly ordered updates
+
+        Args:
+            remaining_updates: list of unordered updates
+            rules: dictionary containing rules for each number
+        
+        Returns:
+            fixed_updates: list of ordered updates
+            result_list: list of bools showing correctly ordered updates 
+    '''
+
+    fixed_updates = []
+
+    for update in remaining_updates:
+
+        fixed_updates.append(fix_incorrect_update(update, rules))
+
+    result_list = [1] * len(fixed_updates)
+
+    return fixed_updates, result_list
+
 def run_day5():
     '''
         This function solves the task for day 3
@@ -106,7 +180,11 @@ def run_day5():
     updates, rules = format_data(data)
     results = check_updates(updates, rules)
     print(f'The sum of middle page number of all correctly ordered updates: {result_sum(updates, results)}')
-   
+    
+    remaining_updates = incorrect_updates(updates, results)
+    fixed_updates, results_fixed = fix_incorrect_updates(remaining_updates, rules)
+    print(f'The sum of middle page number of all correctly ordered updates: {result_sum(fixed_updates, results_fixed)}')
+
 if __name__ == "__main__":
 
     from day3_lib import import_data_day3
