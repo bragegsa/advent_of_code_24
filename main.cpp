f309b402f3fb63560939ebec3e51b8f5a088f4d1
bragega <brage.samsonsen@gmail.com>
2025-01-29 12:23:47 +0100

    Timing (runtime)

--- main.cpp
+++ main.cpp
@@ -10,11 +10,18 @@ int main() {
     std::cout << "--- Day 6 ---" << std::endl;
 
     std::string filepath = "inputs/day6.txt";
+    // std::string filepath = "inputs/inputs_test/day6_test.txt";
+    
     std::vector<std::vector<char>> guard_matrix = read_file(filepath);
 
     std::vector<int> start_position = guard_position(guard_matrix);
 
+    auto start = std::chrono::high_resolution_clock::now();
     guard_patrol(guard_matrix);
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double> duration = end - start;
+    std::cout << "Execution time: " << duration.count() << " seconds" << std::endl;
 
     return 0;
 }
--- main.h
+++ main.h
@@ -4,5 +4,6 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <chrono>
 
 #endif
